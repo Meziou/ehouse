9b1fa711edde7417c2bba289b088a7c400586c27
CMeziou <chems.meziou@gmail.com>
2024-01-30 00:48:24 +0100

    Fix bug

--- src/main/java/com/ehouse_spring/ehouse/entity/repository/interfaces/CrudRepository 2.java
+++
@@ -1,5 +0,0 @@
-package com.ehouse_spring.ehouse.entity.repository.interfaces;
-
-public class CrudRepository {
-
-}
--- src/main/java/com/ehouse_spring/ehouse/entity/repository/interfaces/DeviceRepository.java
+++ src/main/java/com/ehouse_spring/ehouse/entity/repository/interfaces/DeviceRepository.java
@@ -3,7 +3,6 @@ package com.ehouse_spring.ehouse.entity.repository.interfaces;
 import java.util.List;
 
 import com.ehouse_spring.ehouse.entity.Device;
-import com.ehouse_spring.ehouse.entity.repository.interfaces.CrudRepository;
 
 public interface DeviceRepository extends CrudRepository<Device>{
     List<Device> findByRoom(String room);
